14753c1ee9cd037b71367586ead0707bb05d8c40
dumialex32 <dumitrasalexandru26@gmail.com>
2025-04-20 03:44:44 +0200

    feat(auth): implement jwt auth strategy and guard

---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,37 @@
+// jwt.strategy.ts
+import { Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express'; // Import express Request type
+
+// Define a return type for the validation
+interface JwtPayload {
+  sub: string; // or userId
+  steamId: string;
+}
+
+// Create an interface for the request with cookies
+interface AuthenticatedRequest extends Request {
+  cookies: { jwt: string };
+}
+
+interface UserPayload {
+  userId: string;
+  steamId: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: (req: AuthenticatedRequest) => req.cookies?.jwt || null,
+      secretOrKey: configService.get('JWT_SECRET') || 'defaultSecretKey',
+    });
+  }
+
+  validate(payload: JwtPayload): UserPayload {
+    // Safely return the payload with the correct type
+    return { userId: payload.sub, steamId: payload.steamId };
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: 'http://localhost:5713', // Allow only frontend domain
+    credentials: true, // Allow cookies to be sent with requests
+  });
+
+  await app.listen(3000);
 }
-bootstrap();
+
+// Await bootstrap to handle the promise
+bootstrap().catch((error) => {
+  console.error('Error starting the app:', error);
+});
