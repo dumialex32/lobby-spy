033d57714f7a111db33fb698c2c8132a6d2c19ae
dumialex32 <dumitrasalexandru26@gmail.com>
2025-04-22 18:53:13 +0200

    feat(lobb): add basic validation for createLobby

--- backend/src/lobby/dto/create-lobby.dto.ts
+++ backend/src/lobby/dto/create-lobby.dto.ts
@@ -1,7 +1,31 @@
-import { IsString, MinLength } from 'class-validator';
+import {
+  IsEnum,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export enum LobbyVisibility {
+  PUBLIC = 'PUBLIC',
+  PRIVATE = 'PRIVATE',
+}
 
 export class CreateLobbyDto {
   @IsString()
   @MinLength(3)
   name: string;
+
+  @IsEnum(LobbyVisibility)
+  @IsOptional()
+  visibility?: LobbyVisibility;
+
+  @IsString()
+  @IsOptional()
+  avatarUrl?: string;
+
+  @IsString()
+  @MinLength(5)
+  @MaxLength(50)
+  description?: string;
 }
