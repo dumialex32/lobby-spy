882f86bc2e8a2d9787c2cbb1a3be68fa10407848
dumialex32 <dumitrasalexandru26@gmail.com>
2025-04-22 19:26:37 +0200

    feat(lobby): add core endpoints (create/join/get/update visibility)

--- backend/src/lobby/lobby.controller.ts
+++ backend/src/lobby/lobby.controller.ts
@@ -1,6 +1,54 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreateLobbyDto } from './dto/create-lobby.dto';
+import { LobbyService } from './lobby.service';
+import { IsLobbyOwnerGuard } from './guards/is-lobby-owner.guard';
 
 @Controller('lobby')
 @UseGuards(JwtAuthGuard)
-export class LobbyController {}
+export class LobbyController {
+  constructor(private readonly lobbyService: LobbyService) {}
+
+  @Post('create')
+  createLobby(@Body() dto: CreateLobbyDto, @Req() req) {
+    return this.lobbyService.createLobby(dto, req.user.steamId);
+  }
+
+  @Post('join/:lobbyId')
+  joinLobby(@Param('lobbyId') lobbyId: string, @Req() req) {
+    return this.lobbyService.joinLobby(lobbyId, req.user.steamId);
+  }
+
+  @Get('/myLobby')
+  getMyLobby(@Req() req) {
+    return this.lobbyService.getMyLobby(req.user.steamId);
+  }
+
+  @Get('/:lobbyId')
+  getLobby(@Param('lobbyId') lobbyId: string, @Req() req) {
+    return this.lobbyService.getLobby(lobbyId, req.user.steamId);
+  }
+
+  @Patch('/:lobbyId/visibility')
+  @UseGuards(JwtAuthGuard, IsLobbyOwnerGuard)
+  updateLobbyVisibility(
+    @Param('lobbyId') lobbyId: string,
+    @Body() body: { visibility: 'PUBLIC' | 'PRIVATE' },
+    @Req() req,
+  ) {
+    return this.lobbyService.updateLobbyVisibility(
+      lobbyId,
+      req.user.steamId,
+      body.visibility,
+    );
+  }
+}
