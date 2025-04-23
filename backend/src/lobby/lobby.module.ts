4319be1c54011268f59142cb8821316a815df3f2
dumialex32 <dumitrasalexandru26@gmail.com>
2025-04-23 02:56:18 +0200

    chore: update clarity submodule and lobby module

--- backend/src/lobby/lobby.module.ts
+++ backend/src/lobby/lobby.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { LobbyController } from './lobby.controller';
 import { LobbyService } from './lobby.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [LobbyController],
   providers: [LobbyService, PrismaService],
 })
